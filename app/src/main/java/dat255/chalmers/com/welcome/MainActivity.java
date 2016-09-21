a8ea8d6481d0a7c3ee338949d9fadd77b14535f1
Jennifer Linder <Jennifer Linder>
2016-09-21 16:30:14 +0200

    first try

--- app/src/main/java/dat255/chalmers/com/welcome/MainActivity.java
+++ app/src/main/java/dat255/chalmers/com/welcome/MainActivity.java
@@ -2,12 +2,28 @@ package dat255.chalmers.com.welcome;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<String> matchList = new ArrayList<String>();
+    ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, matchList);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView listView = (ListView) findViewById(R.id.listView);
+        listView.setAdapter(itemsAdapter);
+    }
+
+    public void showMatch(View view){
+        itemsAdapter.add("Anders");
+        
     }
 }
