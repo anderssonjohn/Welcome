6d6dc4d9f41fb3d0826be487f154106a808d5e13
daRaha <da_raha@hotmail.com>
2016-09-22 17:06:02 +0200

    Add chat field and button and send method

--- app/src/main/java/dat255/chalmers/com/welcome/ChatActivity.java
+++ app/src/main/java/dat255/chalmers/com/welcome/ChatActivity.java
@@ -2,6 +2,7 @@ package dat255.chalmers.com.welcome;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class ChatActivity extends AppCompatActivity {
 
@@ -9,5 +10,10 @@ public class ChatActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
+    }
+
+    public void sendMessage(View view){
+
     }
 }
