04f4770609929aa68bd7d74a52e7d9ed6a45ea9d
Elin <elin.bjornsson2@gmail.com>
2016-09-23 16:54:52 +0200

    Added InfoActivity

--- app/src/main/java/dat255/chalmers/com/welcome/LanguageActivity.java
+++ app/src/main/java/dat255/chalmers/com/welcome/LanguageActivity.java
@@ -1,7 +1,9 @@
 package dat255.chalmers.com.welcome;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class LanguageActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class LanguageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_language);
+    }
+
+    public void showInfoActivity(View view) {
+        Intent intent = new Intent(LanguageActivity.this, InfoActivity.class);
     }
 }
