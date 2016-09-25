e46f902d99127bb97c2bb345758f72d2f402ef57
Elin <elin.bjornsson2@gmail.com>
2016-09-25 17:05:35 +0200

    Make the confirmation button clickable when a radio button has been clicked

--- app/src/main/java/dat255/chalmers/com/welcome/GenderAndBirthActivity.java
+++ app/src/main/java/dat255/chalmers/com/welcome/GenderAndBirthActivity.java
@@ -2,6 +2,8 @@ package dat255.chalmers.com.welcome;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class GenderAndBirthActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class GenderAndBirthActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gender_and_birth);
+    }
+
+    public void enableButtonNext(View view) {
+        Button b = (Button)findViewById(R.id.buttonNext);
+        b.setClickable(true);
     }
 }
