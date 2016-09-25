e3d3d414faf674f51a9be45d94866d10d0a5acc9
Elin <elin.bjornsson2@gmail.com>
2016-09-25 15:11:13 +0200

    when a button is pressed in InfoActivity you go to GenderAndBirthActivity

--- app/src/main/java/dat255/chalmers/com/welcome/InfoActivity.java
+++ app/src/main/java/dat255/chalmers/com/welcome/InfoActivity.java
@@ -1,7 +1,9 @@
 package dat255.chalmers.com.welcome;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class InfoActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class InfoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
+    }
+
+    public void showGenderAndBirthActivity(View view) {
+        Intent intent = new Intent(InfoActivity.this, GenderAndBirthActivity.class);
+        startActivity(intent);
     }
 }
