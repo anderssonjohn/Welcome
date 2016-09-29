12bb4a81bea1d9b78c8c65a6e2efd31bdf0dd26f
Johan Blomberg <jccblomberg@gmail.com>
2016-09-29 20:29:30 +0200

    Filled in JobAsActivity based on JobSvActivity

--- app/src/main/java/dat255/chalmers/com/welcome/JobAsActivity.java
+++ app/src/main/java/dat255/chalmers/com/welcome/JobAsActivity.java
@@ -1,13 +1,94 @@
 package dat255.chalmers.com.welcome;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+
+import static dat255.chalmers.com.welcome.SharedPreferencesKeys.PREFS_NAME;
+import static dat255.chalmers.com.welcome.SharedPreferencesKeys.JOB_ID;
+import static dat255.chalmers.com.welcome.SharedPreferencesKeys.INTEREST_ID;
+import static dat255.chalmers.com.welcome.SharedPreferencesKeys.FIRST_RUN;
 
 public class JobAsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_job_as);
+        setContentView(R.layout.activity_job);
+
+        Spinner spinnerJ = (Spinner) findViewById(R.id.spinnerJob);
+        Spinner spinnerI = (Spinner) findViewById(R.id.spinnerInterest);
+
+        //Define our own listener that makes sure we can't progress to the next activity
+        //while the empty first item is selected
+        AdapterView.OnItemSelectedListener listener = new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Button button = (Button) findViewById(R.id.buttonDone);
+                if (id == 0) {
+                    button.setEnabled(false);
+                }
+                else {
+                    button.setEnabled(true);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        };
+
+        spinnerJ.setOnItemSelectedListener(listener);
+        spinnerI.setOnItemSelectedListener(listener);
+
+        //Populate the spinners with options via an ArrayAdapter
+        ArrayAdapter<CharSequence> adapter;
+
+        adapter = ArrayAdapter.createFromResource(this,
+                R.array.job_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerJ.setAdapter(adapter);
+
+        adapter = ArrayAdapter.createFromResource(this,
+                R.array.interests_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerI.setAdapter(adapter);
+    }
+
+    private void saveInfo() {
+        Spinner spinnerJ = (Spinner) findViewById(R.id.spinnerJob);
+        Spinner spinnerI = (Spinner) findViewById(R.id.spinnerInterest);
+
+        long jobID = spinnerJ.getSelectedItemId();
+        long interestID = spinnerI.getSelectedItemId();
+        System.out.println(jobID);
+        System.out.println(interestID);
+
+        SharedPreferences prefs = getSharedPreferences(PREFS_NAME, 0);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putLong(JOB_ID, jobID);
+        editor.putLong(INTEREST_ID, interestID);
+        editor.commit();
+    }
+
+    public void showMainActivity(View view) {
+        //Save all data
+        saveInfo();
+
+        //Save that the user has gone through the first time setup
+        SharedPreferences prefs = getSharedPreferences(PREFS_NAME, 0);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putBoolean(FIRST_RUN, false);
+        editor.commit();
+
+        Intent intent = new Intent(JobAsActivity.this, MainActivity.class);
+        startActivity(intent);
     }
 }
