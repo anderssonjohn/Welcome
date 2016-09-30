7ec4ef07ef733ad6f8a6d98fb20aacf098a06a2d
daRaha <da_raha@hotmail.com>
2016-09-30 11:57:22 +0200

    Save data to database

--- app/src/main/java/dat255/chalmers/com/welcome/JobActivity.java
+++ app/src/main/java/dat255/chalmers/com/welcome/JobActivity.java
@@ -2,16 +2,29 @@ package dat255.chalmers.com.welcome;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
 
-import dat255.chalmers.com.welcome.Backend.CreateUser;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
+import static dat255.chalmers.com.welcome.SharedPreferencesKeys.DOB_DAY;
+import static dat255.chalmers.com.welcome.SharedPreferencesKeys.DOB_MONTH;
+import static dat255.chalmers.com.welcome.SharedPreferencesKeys.DOB_YEAR;
+import static dat255.chalmers.com.welcome.SharedPreferencesKeys.GENDER;
+import static dat255.chalmers.com.welcome.SharedPreferencesKeys.LANGUAGE;
 import static dat255.chalmers.com.welcome.SharedPreferencesKeys.PREFS_NAME;
 import static dat255.chalmers.com.welcome.SharedPreferencesKeys.JOB_ID;
 import static dat255.chalmers.com.welcome.SharedPreferencesKeys.INTEREST_ID;
@@ -99,4 +112,78 @@ public class JobActivity extends AppCompatActivity {
         Intent intent = new Intent(JobActivity.this, MainActivity.class);
         startActivity(intent);
     }
+    public class CreateUser extends AsyncTask<String, Void, Void> {
+        @Override
+        protected Void doInBackground(String... strings) {
+
+            String dataUrl = "http://johnandersson.me:3030/user";
+            SharedPreferences preferences = getSharedPreferences(PREFS_NAME, 0);
+
+            String gender = preferences.getString(GENDER, "null");
+            Long profession = preferences.getLong(JOB_ID,0);
+            Long interest = preferences.getLong(INTEREST_ID,0);
+
+            int DateOfBirth = preferences.getInt(DOB_DAY,0);
+            int MonthOfBirth = preferences.getInt(DOB_MONTH,0);
+            int YearOfBirth = preferences.getInt(DOB_YEAR, 0);
+
+
+
+
+            String dataUrlParameters = "name=" + "Johny Awesome"
+                    + "&gender=" + gender
+                    +"&profession=" + profession
+                    + "&interest=" + interest
+                    + "&date=" + YearOfBirth + "-" + MonthOfBirth + "-" + DateOfBirth
+                    + "&swedish_speaker=" + "true";
+
+
+            URL url;
+            HttpURLConnection connection = null;
+            try {
+                // Create connection
+                url = new URL(dataUrl);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.setRequestMethod("POST");
+                connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+                connection.setUseCaches(false);
+                connection.setDoInput(true);
+                connection.setDoOutput(true);
+                connection.setRequestProperty("Accept","*/*");
+
+                // Send request
+                DataOutputStream wr = new DataOutputStream(
+                        connection.getOutputStream());
+                wr.writeBytes(dataUrlParameters);
+                wr.flush();
+                wr.close();
+                // Get Response
+                InputStream is = connection.getInputStream();
+                BufferedReader rd = new BufferedReader(new InputStreamReader(is));
+                String line;
+                StringBuffer response = new StringBuffer();
+                while ((line = rd.readLine()) != null) {
+                    response.append(line);
+                    response.append('\r');
+                }
+                rd.close();
+                String responseStr = response.toString();
+                Log.d("Server response", responseStr);
+
+            } catch (Exception e) {
+
+                e.printStackTrace();
+
+            } finally {
+
+                if (connection != null) {
+                    connection.disconnect();
+                }
+            }
+
+            return null;
+        }
+    }
+
+
 }
