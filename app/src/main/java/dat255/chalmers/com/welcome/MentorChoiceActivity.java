6fd10d70e09c5154891fc22f3279ea230eb4aa8e
Elin <elin.bjornsson2@gmail.com>
2016-10-02 12:20:08 +0200

    save if the user is a mentor or not

--- app/src/main/java/dat255/chalmers/com/welcome/MentorChoiceActivity.java
+++ app/src/main/java/dat255/chalmers/com/welcome/MentorChoiceActivity.java
@@ -1,9 +1,13 @@
 package dat255.chalmers.com.welcome;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+
+import static dat255.chalmers.com.welcome.SharedPreferencesKeys.MENTOR;
+import static dat255.chalmers.com.welcome.SharedPreferencesKeys.PREFS_NAME;
 
 public class MentorChoiceActivity extends AppCompatActivity {
 
@@ -14,12 +18,22 @@ public class MentorChoiceActivity extends AppCompatActivity {
     }
 
     public void showInfoSvActivity(View view) {
+        saveInfo(true);
         Intent intent = new Intent(this, InfoSvActivity.class);
         startActivity(intent);
     }
 
     public void showInfoAsActivity(View view) {
+        saveInfo(false);
         Intent intent = new Intent(this, InfoAsActivity.class);
         startActivity(intent);
+    }
+
+    private void saveInfo(boolean boo) {
+        SharedPreferences prefs = getSharedPreferences(PREFS_NAME, 0);
+        SharedPreferences.Editor editor = prefs.edit();
+
+        editor.putBoolean(MENTOR, boo);
+        editor.commit();
     }
 }
