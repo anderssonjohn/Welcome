73dc5f02cf5cca759acd94ff3c3340af9c3e3428
Johan Blomberg <jccblomberg@gmail.com>
2016-10-02 15:18:37 +0200

    Add a Settings button to the toolbar

--- app/src/main/java/dat255/chalmers/com/welcome/MainActivity.java
+++ app/src/main/java/dat255/chalmers/com/welcome/MainActivity.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -67,13 +70,20 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    public void showSettings(MenuItem item) {
+        Intent intent = new Intent(this, ProfileSettingsActivity.class);
+        startActivity(intent);
+    }
+
     @Override
     public void onBackPressed() {
         //Do nothing
     }
 
-    public void showSettings(View view) {
-        Intent intent = new Intent(this, ProfileSettingsActivity.class);
-        startActivity(intent);
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
