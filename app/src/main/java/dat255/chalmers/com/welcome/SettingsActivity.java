55afac5a453eac707f3d569a01784ef6726854e4
daRaha <da_raha@hotmail.com>
2016-10-10 18:36:49 +0200

    language can be changed in first view

--- app/src/main/java/dat255/chalmers/com/welcome/SettingsActivity.java
+++ app/src/main/java/dat255/chalmers/com/welcome/SettingsActivity.java
@@ -17,7 +17,7 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
-        
+
     }
 
     public static class LanguageDialog extends android.app.DialogFragment{
