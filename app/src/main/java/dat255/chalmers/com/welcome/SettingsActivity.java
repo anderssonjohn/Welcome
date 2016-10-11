45a0145d71bcb788cf5f48ae323b5ed93f15dbe3
Jennifer Linder <jennifer.linder.jl@gmail.com>
2016-10-11 09:36:21 +0200

    SettingsActivity: Can change job, and saves it to the SharedPreferences

--- app/src/main/java/dat255/chalmers/com/welcome/SettingsActivity.java
+++ app/src/main/java/dat255/chalmers/com/welcome/SettingsActivity.java
@@ -2,6 +2,7 @@ package dat255.chalmers.com.welcome;
 
 import android.app.Dialog;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.content.DialogInterface;
@@ -9,6 +10,8 @@ import android.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import java.util.Locale;
+
+import static dat255.chalmers.com.welcome.SharedPreferencesKeys.JOB_ID;
 
 
 public class SettingsActivity extends AppCompatActivity {
@@ -22,9 +25,11 @@ public class SettingsActivity extends AppCompatActivity {
 
     public static class LanguageDialog extends android.app.DialogFragment{
         AlertDialog levelDialog;
+        String languageCode;
 
         @Override
         public Dialog onCreateDialog(Bundle savedInstanceState){
+
 
             // Language options in dialog with radio buttons
             final CharSequence[] items = {getString(R.string.lang_sv),getString(R.string.lang_en),
@@ -39,22 +44,20 @@ public class SettingsActivity extends AppCompatActivity {
                     switch(item)
                     {
                         case 0:
-                            setConfigLanguage("Sv");
-                            updateActivity();
+                            languageCode = "Sv" ;
                             break;
                         case 1:
-                            setConfigLanguage("En");
-                            updateActivity();
+                            languageCode = "En" ;
                             break;
                         case 2:
-                            setConfigLanguage("Pe");
-                            updateActivity();
+                            languageCode = "Pe" ;
                             break;
                         case 3:
-                            setConfigLanguage("Ar");
-                            updateActivity();
+                            languageCode = "Ar" ;
                             break;
                     }
+                    setConfigLanguage(languageCode);
+                    updateActivity();
                     levelDialog.dismiss();
                 }
             });
@@ -84,6 +87,59 @@ public class SettingsActivity extends AppCompatActivity {
     public void changeLanguageOnClick(View view){
         LanguageDialog languageDialog = new LanguageDialog();
         languageDialog.show(getFragmentManager(),"");
+    }
+
+    public static class JobDialog extends android.app.DialogFragment {
+        AlertDialog levelDialog;
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+            // Language options in dialog with radio buttons
+            final CharSequence[] items = {"", "Pizzabagare", "Astrofysiker",
+                    "Arbterslös"};
+
+            // Creating and Building the Dialog
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+            builder.setTitle(R.string.choose_job);
+            builder.setSingleChoiceItems(items, -1, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int item) {
+                    SharedPreferences prefs = getActivity().getPreferences(MODE_PRIVATE);
+                    SharedPreferences.Editor editor = prefs.edit();
+
+                    switch (item) {
+                        case 0:
+                            editor.putString(JOB_ID ,"");
+                            break;
+                        case 1:
+                            editor.putString(JOB_ID ,"Pizzabagare");
+                            break;
+                        case 2:
+                            editor.putString(JOB_ID ,"Astrofysiker");
+                            break;
+                        case 3:
+                            editor.putString(JOB_ID ,"Arbterslös");
+                            break;
+                    }
+                    editor.apply();
+                    levelDialog.dismiss();
+                }
+            });
+
+            levelDialog = builder.create();
+            return levelDialog;
+        }
+    }
+
+
+    public void changeJobOnClick(View view){
+        JobDialog jobDialog = new JobDialog();
+        jobDialog.show(getFragmentManager(),"");
+    }
+
+    public void changeInterestsOnClick(View view){
+
     }
 
 }
